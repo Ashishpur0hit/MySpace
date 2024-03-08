0792e3b39ac0b413fa8652ea69efd4edd431da6b
AASHISH <aashish806@outlook.com>
2024-03-08 16:41:37 +0530

    Authentication Complete

--- app/src/main/java/com/example/myspace/LogIn.java
+++ app/src/main/java/com/example/myspace/LogIn.java
@@ -1,24 +1,84 @@
 package com.example.myspace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.myspace.databinding.ActivityLogInBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogIn extends AppCompatActivity {
 
     private ActivityLogInBinding binding;
+
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private  String Email,Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLogInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        auth=FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();
 
+        //----------------------------- -------------------------------------avoid user to ReLogin-------------------------------------------------------------------//
+        if(user!=null)
+        {
+            Intent it = new Intent(LogIn.this,MainActivity.class);
+            startActivity(it);
+            finish();
+        }
+
+        //-------------------------------------------------------------------LogIn button ------------------------------------------------------------------------------------//
         binding.btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Email = binding.etLogInEmail.getText().toString();
+                Password = binding.etLogInPassword.getText().toString();
+
+                if(!Email.isEmpty() && !Password.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(Email).matches() )
+                {
+                    LogInUser();
+                }
+                else Toast.makeText(LogIn.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+            }
+
+            private void LogInUser() {
+
+                auth.signInWithEmailAndPassword(Email,Password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(LogIn.this, "LogIn Successfull", Toast.LENGTH_SHORT).show();
+                        Intent it = new Intent(LogIn.this,MainActivity.class);
+                        it.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(it);
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LogIn.this, "LogIn Failed"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        });
+
+        //---------------------------------------------------------------New User------------------------------------------------//
+        binding.NewUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LogIn.this,SignUp.class));
--- app/src/main/java/com/example/myspace/SignUp.java
+++ app/src/main/java/com/example/myspace/SignUp.java
@@ -1,16 +1,34 @@
 package com.example.myspace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.myspace.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class SignUp extends AppCompatActivity {
 
     private ActivitySignUpBinding binding;
+    private final FirebaseAuth auth = FirebaseAuth.getInstance();
+    private String FirstName,LastName,Email,Password,UID;
+
+
+    private DatabaseReference User_ref,curr_user;
+    private final FirebaseDatabase DB = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +36,93 @@ public class SignUp extends AppCompatActivity {
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // --------------------------------------------------- Registering User and saving Details ------------------------------------------------------------------------//
 
         binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirstName = binding.etFirstName.getText().toString();
+                LastName = binding.etLastName.getText().toString();
+                Email = binding.etRegisterEmail.getText().toString();
+                Password = binding.etRegisterPassword.getText().toString();
+
+                if((!FirstName.isEmpty() || !LastName.isEmpty()) && !Email.isEmpty() && !Password.isEmpty())
+                {
+                    if(Patterns.EMAIL_ADDRESS.matcher(Email).matches()  && Password.length()>=8)
+                    {
+                        CreateUser();
+                    }
+                    else if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches() ) binding.etRegisterEmail.setError("Enter a vaild Email");
+                    else binding.etRegisterPassword.setError("Password should be atleast of 8 characters");
+                }
+                else if(FirstName.isEmpty())
+                {
+                    binding.etFirstName.setError("Empty Feild are not allowed");
+                }
+                else if(Email.isEmpty()) binding.etRegisterEmail.setError("Empty Feilds are not allowed");
+                else if(LastName.isEmpty()) binding.etRegisterPassword.setError("Empty Felids are not allowed");
+                else Toast.makeText(SignUp.this, "Fill Details Properly", Toast.LENGTH_SHORT).show();
+
+
+            }
+//----------------------------------------------------------Creating User----------------------------------------------------------------------------------------------------//
+
+            private void CreateUser() {
+
+                auth.createUserWithEmailAndPassword(Email , Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(SignUp.this, "Registration Successfull", Toast.LENGTH_SHORT).show();
+                            User_ref = DB.getReference().child("Users");
+                            UID = Objects.requireNonNull(auth.getCurrentUser()).getUid();
+                            curr_user = User_ref.child(UID);
+                            StoreData();
+                            Intent it = new Intent(SignUp.this , LogIn.class);
+                            startActivity(it);
+
+                        }
+                        else
+                        {
+                            Toast.makeText(SignUp.this, "registration Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+//----------------------------------------------------------------------------Storing User Profile ----------------------------------------------------------------------------//
+                    private void StoreData() {
+
+                        String Profile = "";
+
+                        HashMap<String , String> UserMap = new HashMap<>();
+
+                        UserMap.put("UserName" , FirstName+" "+LastName);
+                        UserMap.put("Email" , Email);
+                        UserMap.put("Password" , Password);
+                        UserMap.put("HomeTown" , "");
+                        UserMap.put("Profile" , Profile);
+                        UserMap.put("Gender" , "");
+                        UserMap.put("Bio" , "");
+                        UserMap.put("UID" , UID);
+                        UserMap.put("Relationship Status" , "");
+
+
+                        curr_user.setValue(UserMap);
+                        binding.etLastName.setText("");
+                        binding.etFirstName.setText("");
+                        binding.etRegisterEmail.setText("");
+                        binding.etRegisterPassword.setText("");
+
+
+                    }
+                });
+
+            }
+        });
+
+        //------------------------------------------------Already a User------------------------------------------------------------//
+
+        binding.AlreadyUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SignUp.this, LogIn.class));
